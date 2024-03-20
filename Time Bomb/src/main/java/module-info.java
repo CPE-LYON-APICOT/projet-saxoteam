8d0de5616921892edadd8b7b1ec6fa7e105285de
adribrt <adrien.barrat@cpe.fr>
2024-03-20 16:35:18 +0100

    initialisation du projet

---
+++ Time Bomb/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module timebomb.time_bomb {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    //requires org.controlsfx.controls;
+    //requires com.dlsc.formsfx;
+    //requires net.synedra.validatorfx;
+    //requires org.kordamp.bootstrapfx.core;
+
+    opens timebomb.time_bomb to javafx.fxml;
+    exports timebomb.time_bomb;
+}
