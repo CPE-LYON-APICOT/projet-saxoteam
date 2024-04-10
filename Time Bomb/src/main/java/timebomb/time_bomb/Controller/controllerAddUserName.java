505e0b00854c1fe377c31d8512975c8dc8444b9d
adribrt <adrien.barrat@cpe.fr>
2024-04-10 17:28:11 +0200

    Creation du tableau de jeu + Creation de joueur avec leurs nom

--- Time Bomb/src/main/java/timebomb/time_bomb/Controller/controllerAddUserName.java
+++ Time Bomb/src/main/java/timebomb/time_bomb/Controller/controllerAddUserName.java
@@ -11,6 +11,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import timebomb.time_bomb.Models.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class controllerAddUserName {
 
@@ -35,10 +40,12 @@ public class controllerAddUserName {
         numberLabel.setText("ajouter les nom des "+ number.toString()+" joueurs");
 
         TextField textField = new TextField();
+        textField.setId("id0");
         grid.addRow(0,textField);
         System.out.println(grid.getRowCount());
         for (int i = 1; i < this.number; i++) {
             TextField textFieldAdd = new TextField();
+            textFieldAdd.setId("id"+i);
             grid.addRow(i,textFieldAdd);
         }
     }
@@ -47,8 +54,27 @@ public class controllerAddUserName {
     @FXML
     void quitteHandlers(ActionEvent event) {
         Platform.exit();
+        System.exit(0);
 
     }
 
 
+    public void valideHandlers(ActionEvent actionEvent) {
+        List<Joueur> values = new ArrayList<>(); // Créez une liste pour stocker les valeurs des TextField
+
+        for (int i = 0; i < number; i++) {
+            TextField textField = (TextField) grid.lookup("#id" + i);
+            if (Objects.equals(textField.getText(), "")){
+                return;
+            }
+            values.add(new Joueur(textField.getText()) {
+                @Override
+                public void jouer() {
+                }
+            });
+        }
+
+
+        System.out.println("Valeurs des TextFields : " + values);
+    }
 }
---
+++ Time Bomb/src/main/java/timebomb/time_bomb/Controller/controllerGameTable.java
@@ -0,0 +1,22 @@
+package timebomb.time_bomb.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class controllerGameTable {
+
+    @FXML
+    private HBox hboxSelectCard;
+
+    @FXML
+    private Label joureurActuLabel;
+
+    @FXML
+    private Label resultLabel;
+
+    @FXML
+    private VBox vboxSelectPlayer;
+
+}
